f0f0c3d6ccd78fc71b97ab698ef3711b8092a725
sdrief <sara.drief@eliberty.fr>
2025-10-24 16:52:21 +0200

    Test Flutter Android CI

---
+++ integration_test/app_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ci_demo/main.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Counter increments when tapping the button', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    // Vérifie que le compteur démarre à 0
+    expect(find.text('0'), findsOneWidget);
+
+    // Clique sur le bouton "+"
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpAndSettle();
+
+    // Vérifie que le compteur passe à 1
+    expect(find.text('1'), findsOneWidget);
+  });
+}
